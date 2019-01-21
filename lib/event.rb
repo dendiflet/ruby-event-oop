8097f65010a12b5a2e3b3fecaf694c3fdb314bda
dendiflet <bretin.iannis@gmail.com>
2019-01-21 17:58:46 +0100

    is_soon ok

--- lib/event.rb
+++ lib/event.rb
@@ -15,6 +15,36 @@ class Event
 		@title = title
 		@attendees = attendees
 	end
+
+	def postpone_24h
+		original_date = @start_date
+		@start_date = @start_date + 60 * 60 * 24 # 60secs * 60mn * 24h
+	end
+
+  def end_date
+  	@end_date = @start_date + 60 * @length
+  end
+
+  def is_past
+  	Time.now > @start_date
+  end
+
+  def is_future
+  	!self.is_past
+  end
+
+  def is_soon
+  	soon = @start_date - Time.now < 60 * 30 && start_date - Time.now > 0
+  	if soon == true
+  	  puts "depeche toi"
+    end
+    return soon
+  end
+
+
+
+
+
 end
 
 binding.pry
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,10 +7,23 @@ describe Event do
     load 'event.rb'
   end
 
+		
+		
+
 	describe 'initializer' do
 		it 'creates an event' do
+		#créer un event
+		standup = Event.new("2019-01-13 09:00", 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
+		 expect(standup.class).to eq(Event)
 
 		end
+	end
+
+	describe 'postpone_24h' do
+		it 'move the date one days after the inital date'
+		@start_date = Time.parse("2019-01-13 09:00")
+		 new_date = @start_date + 60 * 60 * 24
+		expect(@start_date).to eq(new_date)
 	end
 
 end
